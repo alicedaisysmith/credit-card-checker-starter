683229c7c7182113612a3a4afd0fe458d9082d99
Alice Smith <alicesmith@Alices-MacBook-Pro.local>
2022-02-20 18:55:26 +0000

    tests added and refactored

---
+++ main.spec.js
@@ -0,0 +1,32 @@
+const {findInvalidCards, idInvalidCardCompanies} = require( "./main");
+
+// All valid credit card numbers
+const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
+const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9];
+const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6];
+const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5];
+const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
+
+// All invalid credit card numbers
+const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
+const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
+const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4];
+const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
+const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4];
+
+
+describe('findInvalidCards',() => {
+    it ('should find invalid card numbers from an array', () => {
+        const invalidCards = findInvalidCards([valid1,valid2,valid3,valid4,valid5,invalid1,invalid2,invalid3,invalid4,invalid5])
+
+        expect(invalidCards).toEqual([invalid1,invalid2,invalid3,invalid4,invalid5])
+    })
+})
+
+describe('idInvalidCardCompanies',() => {
+    it ('should find the companies that the invalid card numbers came from in an array, only producing the company once', () => {
+        const invalidCardCompanies = idInvalidCardCompanies([invalid1,invalid2,invalid3,invalid4,invalid5])
+
+        expect(invalidCardCompanies).toEqual(['Visa','Mastercard','Amex','Discover'])
+    })
+})
